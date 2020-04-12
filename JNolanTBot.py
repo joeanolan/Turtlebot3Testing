73465fbb91a1ebb04e8b0ab3c8a419422d5fc3e8
joeanolan <57333475+joeanolan@users.noreply.github.com>
2020-04-11 19:17:38 -0500

    Update JNolanTBot.py

--- JNolanTBot.py
+++ JNolanTBot.py
@@ -2,79 +2,71 @@
 #
 #
 #
-
 import rospy
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Point
-#from geometry_msgs.msg import Odometry
+import math
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
+from beginner_tutorials.msg import Position
+x=0.0
+y=0.0
 
-import time
+yaw = 0.0
+goal = Point()
+goal.x = 5
+goal.y = 5
+move = Twist()
+yawKp = 0.25
+forwardKp = 0.1
+old_yaw = 0
+num_wraps = 0
 
-twist = Twist()
 
-def callback_pos():
-	position = Point()
-	return position
 
-def callback_odom():
-	odom = Odometry()
-	return odom
+def odom(data,pub):
+  global x
+  global y
+  global old_yaw,num_wraps,yaw
+  threshold = 15*math.pi/180
+  pos_msg = Position()
+  x = data.pose.pose.position.x
+  y = data.pose.pose.position.y
+  rot_quat = data.pose.pose.orientation
+  (roll,pitch,yaw) = euler_from_quaternion ([rot_quat.x,rot_quat.y,rot_quat.z,rot_quat.w])
+  if old_yaw < -threshold and yaw > threshold: # from -pi to pi (increasing negative)
+    num_wraps = num_wraps - 1
+  elif old_yaw > threshold and yaw < -threshold:
+    num_wraps = num_wraps + 1
+  wrapped_yaw = yaw + 2*math.pi*num_wraps
+  old_yaw = yaw
+  pos_msg.angular.roll  = roll * 180/math.pi
+  pos_msg.angular.pitch = pitch * 180/math.pi
+  pos_msg.angular.yaw   = wrapped_yaw * 180/math.pi
+  pub1.publish(pos_msg)
 
-def right():
-	global move_cmd
-	global cmd_vel
-	rospy.loginfo("right")  
-	# turn at 0.15 rad/s
-        move_cmd.angular.z = 0.15
-	move_cmd.linear.x = 0.0
-        cmd_vel.publish(move_cmd)
-        r.sleep()
+rospy.init_node("Controller")
+pub= rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
+pub1 = rospy.Publisher('/eul', Position, queue_size=10)
+sub = rospy.Subscriber('/odom',Odometry,odom,(pub1))
+rate = rospy.Rate(100)
 
-def forward():
-	global move_cmd
-	global cmd_vel
-	rospy.loginfo("forward")
-        move_cmd.linear.x = 1.5
-	move_cmd.angular.z = 0.0
-        cmd_vel.publish(move_cmd)
-        r.sleep()
-                  
-def stop():
-        rospy.loginfo("Stopped")
-	move_cmd.linear.x=0.0
-	move_cmd.angular.z = 0.0
-        cmd_vel.publish(move_cmd)
-   
 
-def main():
-	global twist
-	twist.linear.x = 1.5
-	twist.angular.z = 0.0
-	pub.publish(twist)
-	time.sleep(5)
-	twist.linear.x = 0 
-	twist.angular.z = 0.15
-	pub.publish(twist)
-	time.sleep(2)
-	twist.angular.z=0.0
-	twist.linear.x = 1.5
-	pub.publish(twist)
-	time.sleep(5)
-	twist.linear.x = 0.0
-	pub.publish(twist)
+while not rospy.is_shutdown():
+  deltaX = goal.x - x
+  deltaY = goal.y - y
+  bearingToGoal = math.atan2(deltaX,deltaY) 
+  if goal.x == x and goal.y == y:
+    move.angular.z = 0.0
+    move.linear.x=0.0
+    print("GOAL!")
+    break
+  if abs(bearingToGoal - yaw) > 0.1:
+    move.angular.z= (bearingToGoal - yaw) * yawKp
+    move.linear.x = 0.02
+    print('turning to',bearingToGoal,'from',yaw)
+  else:
+    move.linear.x = ((math.hypot(goal.x-x,goal.y-y)) * forwardKp)
+    print(x,y)
 
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-rospy.Subscriber('/eul',Point,callback_pos)
-#rospy.Subscriber('/odom',Odometry,callback_odom)
-
-rospy.init_node('JNOLAN')
-if __name__ == '__main__':
-	try:
-		print('before while')
-		while not rospy.is_shutdown():
-			main()
-			#pub.publish(twist)
-			
-	except rospy.ROSInterruptException:
-        	rospy.loginfo("node terminated")
-		pass
+  pub.publish(move)
+  rate.sleep()
